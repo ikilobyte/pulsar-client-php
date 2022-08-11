208ac97a229de641fb66766989237831007fa271
sunny <ikilobyte@163.com>
2022-08-11 18:27:39 +0800

    fixed baseCommand method

--- src/Util/TypeParser.php
+++ src/Util/TypeParser.php
@@ -27,6 +27,29 @@ class TypeParser
     public static function parseMethodName(Type $type, string $prefix = 'set'): string
     {
         $method = $prefix;
+
+        $value = $type->value();
+        if ($value == Type::LOOKUP_VALUE) {
+            return 'setLookupTopic';
+        }
+
+        if ($value == Type::REDELIVER_UNACKNOWLEDGED_MESSAGES_VALUE) {
+            return 'setRedeliverUnacknowledgedMessages';
+        }
+
+        // lookupTopicResponse
+        if ($value == Type::LOOKUP_RESPONSE_VALUE) {
+            return 'getLookupTopicResponse';
+        }
+
+        if ($value == Type::PARTITIONED_METADATA_VALUE) {
+            return 'setPartitionMetadata';
+        }
+
+        if ($value == Type::PARTITIONED_METADATA_RESPONSE_VALUE) {
+            return 'getPartitionMetadataResponse';
+        }
+
         foreach (explode('_', $type->name()) as $part) {
             $method .= ucfirst(strtolower($part));
         }
