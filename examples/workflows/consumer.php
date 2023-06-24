b97f12bd77277a50749dda436ac48a471c567ee8
Sunny <ikilobyte@163.com>
2023-06-24 18:19:34 +0800

    add workflows test

---
+++ examples/workflows/consumer.php
@@ -0,0 +1,62 @@
+<?php
+
+use Pulsar\Authentication\Jwt;
+use Pulsar\Consumer;
+use Pulsar\ConsumerOptions;
+use Pulsar\Exception\MessageNotFound;
+use Pulsar\SubscriptionType;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+
+$options = new ConsumerOptions();
+
+$token = file_get_contents(__DIR__ . '/../../.github/jwt.token');
+$options->setAuthentication(new Jwt($token));
+
+$options->setConnectTimeout(3);
+$options->setTopic('persistent://public/default/demo');
+$options->setSubscription('workflows');
+$options->setSubscriptionType(SubscriptionType::Shared);
+$options->setNackRedeliveryDelay(20);
+$consumer = new Consumer('pulsar://localhost:6650', $options);
+$consumer->connect();
+
+$receive = $total = 0;
+
+while (true) {
+
+    if ($total >= 1200) {
+        exit(1);
+    }
+
+    if ($receive >= 20) {
+        break;
+    }
+
+    try {
+        $message = $consumer->receive(false);
+        $receive += 1;
+
+        echo sprintf('Got message 【%s】messageID[%s]  topic[%s] publishTime[%s]',
+                $message->getPayload(),
+                $message->getMessageId(),
+                $message->getTopic(),
+                $message->getPublishTime()
+            ) . "\n";
+
+        $consumer->ack($message);
+
+    } catch (MessageNotFound $e) {
+        if ($e->getCode() == MessageNotFound::Ignore) {
+            continue;
+        }
+        die($e->getMessage());
+    } catch (Throwable $e) {
+        die($e->getMessage());
+    } finally {
+        $total += 1;
+    }
+}
+
+$consumer->close();
---
+++ examples/workflows/producer.php
@@ -0,0 +1,39 @@
+<?php
+
+use Pulsar\Authentication\Jwt;
+use Pulsar\Compression\Compression;
+use Pulsar\MessageOptions;
+use Pulsar\Producer;
+use Pulsar\ProducerOptions;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$options = new ProducerOptions();
+
+$token = file_get_contents(__DIR__ . '/../../.github/jwt.token');
+
+$options->setAuthentication(new Jwt($token));
+
+$options->setInitialSubscriptionName('workflows');
+$options->setConnectTimeout(3);
+$options->setTopic('persistent://public/default/demo');
+$options->setCompression(Compression::ZLIB);
+$producer = new Producer('pulsar://localhost:6650', $options);
+$producer->connect();
+
+for ($i = 0; $i < 10; $i++) {
+    $messageID = $producer->send(sprintf('hello %d', $i));
+    echo sprintf("message id %s \n", $messageID);
+}
+
+// Sending delayed messages
+for ($i = 0; $i < 10; $i++) {
+    $messageID = $producer->send(sprintf('hello-delay %d', $i), [
+        MessageOptions::DELAY_SECONDS => $i * 5, // Seconds
+    ]);
+    echo sprintf("message id %s delay %s\n", $messageID, $i * 5);
+}
+
+// close
+$producer->close();
+
